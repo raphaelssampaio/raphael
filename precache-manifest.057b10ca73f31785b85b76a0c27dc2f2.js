07de1ee6377157acb38af67aa462ed44eb394959
Raphael <raphaelssampaio@gmail.com>
2020-06-01 19:08:44 -0300

    Updates

---
+++ precache-manifest.057b10ca73f31785b85b76a0c27dc2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc215b7fb33e0a92eb42b7b090325492",
+    "url": "/raphael/index.html"
+  },
+  {
+    "revision": "5536d3f176a2a9097821",
+    "url": "/raphael/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6d4071b606eb0c6a1e65",
+    "url": "/raphael/static/js/2.3d61b6ae.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/raphael/static/js/2.3d61b6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5536d3f176a2a9097821",
+    "url": "/raphael/static/js/main.618784e5.chunk.js"
+  },
+  {
+    "revision": "bef996a87e6a498919b1",
+    "url": "/raphael/static/js/runtime-main.696503c9.js"
+  }
+]);
