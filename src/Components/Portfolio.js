3cf21e17fca8cae3ad0118484bc9ff3ab9f4c094
Raphael <raphael.sampaio@dellead.com>
2020-06-02 10:26:56 -0300

    Updating projects title

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -31,7 +31,7 @@ class Portfolio extends Component {
       <section id="portfolio">
         <div className="row">
           <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Check Out Some of My Works as Developer and QA Tester</h1>
 
             <div
               id="portfolio-wrapper"
